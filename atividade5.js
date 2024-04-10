4f22aa3d508d09287aeb08f3897264c5c979a0c2
Luan Souza da Silva <luansouza70690@gmail.com>
2024-04-09 21:27:02 -0300

    Add files via upload

---
+++ atividade5.js
@@ -0,0 +1,16 @@
+let lista = [1,20,53,21,6,7,12,68]
+
+function maiormedia(lista){console.log("Maior q média:") 
+    return lista.filter((valor)=>valor>lista.reduce((valor1,valor2)=>valor1+valor2)/lista.length)}console.log(maiormedia(lista),"\n")
+
+function menor(lista){console.log("Menor:")  
+    return lista.reduce((valor1,valor2)=>valor1<valor2?valor1:valor2,Infinity)}console.log(menor(lista),"\n")
+
+function soma(lista){console.log("Soma:")
+    return lista.reduce((valor1,valor2)=>valor1+valor2,0)}console.log(soma(lista),"\n")
+
+function menor20(lista){console.log("Menor q 20:")
+    return lista.filter((valor)=>valor<20)}console.log(menor20(lista),"\n")
+
+function PeU(lista){console.log("Primeiro e ultimo:")
+    return [lista[0],lista[lista.length-1]]}console.log(PeU(lista),)
